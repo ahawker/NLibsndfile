62c4ce6b8e290bea6f22bc49f248a49919977f11
ahawker <andrew.r.hawker@gmail.com>
2012-03-30 00:56:55 -0700

    Added basic CalcMaxAllChannels, CalcNormMaxAllChannels and GetLibVersion command integration tests.

---
+++ src/NLibsndfile.Native.Tests/Integration/Command/GetLibVersionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NLibsndfile.Native.Tests.Integration.Command
+{
+    [TestFixture]
+    [Category("NLibsndfileApi.Native.IntegrationTests.CommandApi")]
+    public class GetLibVersionTests
+    {
+        [Test]
+        public void GetLibVersion_ReturnsCorrectVersion()
+        {
+            var api = new LibsndfileApi();
+
+            var version = api.Commands.GetLibVersion();
+            Assert.That(!string.IsNullOrEmpty(version));
+        }
+    }
+}
